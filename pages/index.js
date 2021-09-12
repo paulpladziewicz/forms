89cd20d1449228c6591e146bf38aea668178b79b
Paul Pladziewicz <41588646+paulpladziewicz@users.noreply.github.com>
2021-09-12 09:13:04 -0400

    Added prototype description

--- pages/index.js
+++ pages/index.js
@@ -16,9 +16,21 @@ export default function Home() {
 			</Head>
 
 			<h1>Become A Customer</h1>
-			<LangDropdown />
-			{locale === 'en' && <BecomeACustomerForm />}
-			{locale === 'es' && <BecomeACustomerFormES />}
+			<LangDropdown/>
+			{locale === 'en' && <BecomeACustomerForm/>}
+			{locale === 'es' && <BecomeACustomerFormES/>}
+
+			<h2>Prototype Description</h2>
+			<p>This project utilizes the following technologies:</p>
+			<ul>
+				<li>Next.js</li>
+				<li>Formik</li>
+				<li>Yup (form validation)</li>
+				<li>Material UI</li>
+			</ul>
+
+			<p>The project is internationalized for English and Spanish. The correct locale would be chosen based on the user's browser settings, or the user could utilize the language dropdown.</p>
+			<a href="https://github.com/paulpladziewicz/forms" target='_blank' rel="noreferrer">Review the code on Github here.</a>
 		</div>
 	)
 }
