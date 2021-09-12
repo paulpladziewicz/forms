4a65744506b111ca4f7c45850fbcc0bb9cbbae46
Paul Pladziewicz <41588646+paulpladziewicz@users.noreply.github.com>
2021-09-12 08:49:15 -0400

    added fields and error checking

--- components/forms/BecomeACustomerForm.js
+++ components/forms/BecomeACustomerForm.js
@@ -1,22 +1,31 @@
 import { Formik } from 'formik'
-import { TextField } from '@material-ui/core'
+import {FormControl, InputLabel, MenuItem, Select, TextField} from '@material-ui/core'
 import * as yup from 'yup'
 
+const phoneRegExp = new RegExp('^(\\+\\d{1,2}\\s?)?1?\\-?\\.?\\s?\\(?\\d{3}\\)?[\\s.-]?\\d{3}[\\s.-]?\\d{4}$')
+
 const validationSchema = yup.object({
-	email: yup
-		.string()
-		.required()
-		.email(),
 	businessName: yup
 		.string()
 		.required('Business name is required.')
 		.max(10, 'Business name cannot be more than 10 characters.'),
+	businessType: yup
+		.string()
+		.required('Please select a Business Type.'),
 	firstName: yup
 		.string()
 		.required('First name is required.'),
 	lastName: yup
 		.string()
-		.required('Last name is required.')
+		.required('Last name is required.'),
+	email: yup
+		.string()
+		.required('Please provide a valid email address.')
+		.email(),
+	phone: yup
+		.string()
+		.matches(phoneRegExp, 'Please enter a valid phone number.')
+		.required('Please enter a valid phone number.')
 });
 
 const BecomeACustomerForm = () => {
@@ -28,8 +37,11 @@ const BecomeACustomerForm = () => {
 				validationSchema={validationSchema}
 				initialValues={{
 					businessName: '',
+					businessType: '',
 					firstName: '',
-					lastName: ''
+					lastName: '',
+					email: '',
+					phone: ''
 				}}
 				onSubmit={(values, { setSubmitting }) => {
 					setTimeout(() => {
@@ -46,20 +58,33 @@ const BecomeACustomerForm = () => {
 						handleBlur,
 						handleSubmit,
 						isSubmitting,
-						/* and other goodies */
 					}) => (
 					<form onSubmit={handleSubmit} style={{display: 'flex', flexDirection: 'column', width: '500px'}}>
 						<TextField
 							type="text"
 							name="businessName"
 							label="Business or Non-Profit Organization Name"
-							style={{marginBottom: '16px'}}
 							onChange={handleChange}
 							onBlur={handleBlur}
 							value={values.businessName}
 						/>
 						{errors.businessName && touched.businessName && errors.businessName}
-						{/*// dfds*/}
+						<FormControl>
+							<InputLabel id="business-type-label">Business Type</InputLabel>
+							<Select
+								type='select'
+								labelId="business-type-label"
+								name='businessType'
+								value={values.businessType}
+								onChange={handleChange}
+								onBlur={handleBlur}
+							>
+								<MenuItem value='Amusement/Recreation'>Amusement/Recreation</MenuItem>
+								<MenuItem value='Business Service'>Business Service</MenuItem>
+								<MenuItem value='College'>College</MenuItem>
+							</Select>
+						</FormControl>
+						{errors.businessType && touched.businessType && errors.businessType}
 						{/*//*/}
 						<TextField
 							type="text"
@@ -79,9 +104,31 @@ const BecomeACustomerForm = () => {
 							value={values.lastName}
 						/>
 						{errors.lastName && touched.lastName && errors.lastName}
-						<button type="submit" disabled={isSubmitting}>
+						<TextField
+							type="email"
+							name="email"
+							label="Email"
+							onChange={handleChange}
+							onBlur={handleBlur}
+							value={values.email}
+						/>
+						{errors.email && touched.email && errors.email}
+						<TextField
+							type="text"
+							name="phone"
+							label="Phone Number"
+							onChange={handleChange}
+							onBlur={handleBlur}
+							value={values.phone}
+						/>
+						{errors.phone && touched.phone && errors.phone}
+						<button type="submit" disabled={isSubmitting} style={{margin: '16px 0px'}}>
 							Submit
 						</button>
+						<h4 style={{margin: '0'}}>Value Data</h4>
+						<pre>{JSON.stringify(values, null, 2)}</pre>
+						<h4 style={{margin: '0'}}>Error Data</h4>
+						<pre>{JSON.stringify(errors, null, 2)}</pre>
 					</form>
 				)}
 			</Formik>
