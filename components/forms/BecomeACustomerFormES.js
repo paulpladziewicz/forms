808162362ff9d40d4e4d28842a228c95244b5d8f
Paul Pladziewicz <41588646+paulpladziewicz@users.noreply.github.com>
2021-09-12 08:57:58 -0400

    completed Spanish version of the form

--- components/forms/BecomeACustomerFormES.js
+++ components/forms/BecomeACustomerFormES.js
@@ -1,35 +1,47 @@
 import { Formik } from 'formik'
-import { TextField } from '@material-ui/core'
+import {FormControl, InputLabel, MenuItem, Select, TextField} from '@material-ui/core'
 import * as yup from 'yup'
 
+const phoneRegExp = new RegExp('^(\\+\\d{1,2}\\s?)?1?\\-?\\.?\\s?\\(?\\d{3}\\)?[\\s.-]?\\d{3}[\\s.-]?\\d{4}$')
+
 const validationSchema = yup.object({
-	email: yup
-		.string()
-		.required()
-		.email(),
 	businessName: yup
 		.string()
-		.required('El nombre de la empresa es obligatorio.')
-		.max(10, 'Business name cannot be more than 10 characters.'),
+		.required('Se requiere el nombre de la empresa.')
+		.max(10, 'El nombre de la empresa no puede tener más de 10 caracteres.'),
+	businessType: yup
+		.string()
+		.required('Seleccione un tipo de empresa.'),
 	firstName: yup
 		.string()
-		.required('First name is required.'),
+		.required('Se requiere el primer nombre.'),
 	lastName: yup
 		.string()
-		.required('Last name is required.')
+		.required('Se requiere el apellido.'),
+	email: yup
+		.string()
+		.required('Por favor ingrese su dirección de correo electrónico válida.')
+		.email(),
+	phone: yup
+		.string()
+		.matches(phoneRegExp, 'Por favor ingrese un número de teléfono válido.')
+		.required('Por favor ingrese un número de teléfono válido.')
 });
 
 const BecomeACustomerFormES = () => {
 	return (
 		<div>
-			<h2>Form En Español</h2>
+			<h2>Form</h2>
 			<Formik
 				validateOnChange={true}
 				validationSchema={validationSchema}
 				initialValues={{
 					businessName: '',
+					businessType: '',
 					firstName: '',
-					lastName: ''
+					lastName: '',
+					email: '',
+					phone: ''
 				}}
 				onSubmit={(values, { setSubmitting }) => {
 					setTimeout(() => {
@@ -46,42 +58,76 @@ const BecomeACustomerFormES = () => {
 						handleBlur,
 						handleSubmit,
 						isSubmitting,
-						/* and other goodies */
 					}) => (
 					<form onSubmit={handleSubmit} style={{display: 'flex', flexDirection: 'column', width: '500px'}}>
 						<TextField
 							type="text"
 							name="businessName"
-							label="Business or Non-Profit Organization Name"
-							style={{marginBottom: '16px'}}
+							label="Nombre de la organización comercial o sin fines de lucro"
 							onChange={handleChange}
 							onBlur={handleBlur}
 							value={values.businessName}
 						/>
-						{errors.businessName && touched.businessName && errors.businessName}
-						{/*// dfds*/}
-						{/*//*/}
+						<span style={{color: 'red'}}>{errors.businessName && touched.businessName && errors.businessName}</span>
+						<FormControl>
+							<InputLabel id="business-type-label">Tipo de negocio</InputLabel>
+							<Select
+								type='select'
+								labelId="business-type-label"
+								name='businessType'
+								value={values.businessType}
+								onChange={handleChange}
+								onBlur={handleBlur}
+							>
+								<MenuItem value='Amusement/Recreation'>Diversión/Recreación</MenuItem>
+								<MenuItem value='Business Service'>Servicio empresarial</MenuItem>
+								<MenuItem value='College'>Universidad</MenuItem>
+							</Select>
+						</FormControl>
+						<span style={{color: 'red'}}>{errors.businessType && touched.businessType && errors.businessType}</span>
 						<TextField
 							type="text"
 							name="firstName"
-							label="First Name"
+							label="Primer nombre"
 							onChange={handleChange}
 							onBlur={handleBlur}
 							value={values.firstName}
 						/>
-						{errors.firstName && touched.firstName && errors.firstName}
+						<span style={{color: 'red'}}>{errors.firstName && touched.firstName && errors.firstName}</span>
 						<TextField
 							type="text"
 							name="lastName"
-							label="Last Name"
+							label="Apellido"
 							onChange={handleChange}
 							onBlur={handleBlur}
 							value={values.lastName}
 						/>
-						{errors.lastName && touched.lastName && errors.lastName}
-						<button type="submit" disabled={isSubmitting}>
-							Submit
+						<span style={{color: 'red'}}>{errors.lastName && touched.lastName && errors.lastName}</span>
+						<TextField
+							type="email"
+							name="email"
+							label="Correo electrónico"
+							onChange={handleChange}
+							onBlur={handleBlur}
+							value={values.email}
+						/>
+						<span style={{color: 'red'}}>{errors.email && touched.email && errors.email}</span>
+						<TextField
+							type="text"
+							name="phone"
+							label="Número de teléfono"
+							onChange={handleChange}
+							onBlur={handleBlur}
+							value={values.phone}
+						/>
+						<span style={{color: 'red'}}>{errors.phone && touched.phone && errors.phone}</span>
+						<button type="submit" disabled={isSubmitting} style={{margin: '16px 0px'}}>
+							Enviar
 						</button>
+						<h3 style={{margin: '0'}}>Value Data</h3>
+						<pre style={{color: 'blue'}}>{JSON.stringify(values, null, 2)}</pre>
+						<h3 style={{margin: '0'}}>Error Data</h3>
+						<pre style={{color: 'blue'}}>{JSON.stringify(errors, null, 2)}</pre>
 					</form>
 				)}
 			</Formik>
